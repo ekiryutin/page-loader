92ee37789e970e8df0d12ebba224757fdff1e599
Eduard Kiryutin <kiryutin@yandex.ru>
2019-01-08 05:29:14 +0200

    fix test

--- __tests__/test.js
+++ __tests__/test.js
@@ -7,8 +7,8 @@ import downloadPage from '../src';
 const expectedFile = path.resolve(__dirname, '__fixtures__/test.html');
 
 const server = 'https://host';
-const page = '/test';
-const testUrl = `${server}${page}`;
+// const page = '/test';
+const pages = ['/test', '/test/'];
 
 let outputDir;
 
@@ -16,19 +16,22 @@ beforeAll(async () => {
   outputDir = await fs.promises.mkdtemp(path.join(os.tmpdir(), path.sep));
 });
 
-test(`download ${page}`, async () => {
-  nock(server)
-    .get(page)
-    .replyWithFile(
-      200,
-      expectedFile,
-      { 'Content-Type': 'text/html' },
-    );
-  await downloadPage(testUrl, outputDir);
+pages.forEach((page) => {
+  test(`download ${page}`, async () => {
+    nock(server)
+      .get(page)
+      .replyWithFile(
+        200,
+        expectedFile,
+        { 'Content-Type': 'text/html' },
+      );
+    const testUrl = `${server}${page}`;
+    await downloadPage(testUrl, outputDir);
 
-  const expected = await fs.promises.readFile(expectedFile);
-  const actual = await fs.promises.readFile(`${outputDir}/host-test.html`);
+    const expected = await fs.promises.readFile(expectedFile);
+    const actual = await fs.promises.readFile(`${outputDir}/host-test.html`);
 
-  // expect(actual).toBe(expected);
-  expect(actual.equals(expected)).toBe(true);
+    // expect(actual).toBe(expected);
+    expect(actual.equals(expected)).toBe(true);
+  });
 });
